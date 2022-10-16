423bca6cac57732426e0c0ea76d38c9798bd0774
Walid Mohamed <walidridge1@gmail.com>
2022-10-16 08:33:40 +0300

    final commit

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = process.env.ATLAS_URI;
+const uri = process.env.MONGO_URI;
 mongoose.connect(uri);
 
 const connection = mongoose.connection;
@@ -26,9 +26,9 @@ app.use('/api/user', userRoutes);
 
 if (process.env.NODE_ENV === 'production'){
     app.use(express.static('staff-frontend/build'))
-    // app.get('*', (req, res) =>{
-    //     res.sendFile(path.resolve(__dirname, 'staff-frontend', 'build', 'index.html'))
-    // })
+    app.get('*', (req, res) =>{
+        res.sendFile(path.resolve(__dirname, 'staff-frontend', 'build', 'index.html'))
+    })
 }
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`)
